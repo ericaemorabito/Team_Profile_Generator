a9efc5b15ad7f3c05356f6b50e3eb7fa7712792e
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-10-05 16:19:33 -0500

    post tutor sesion updates

--- generateHtml.js
+++ generateHtml.js
@@ -1,5 +1,8 @@
 //The HTML template literal
 
+//newManager.name 
+//! from the team array you populate the template literal
+
 const generateHTML = function(data){
   console.log(data)
   //return `${data}`
--- index.js
+++ index.js
@@ -1,29 +1,78 @@
 //Import dependencies
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {generateHTML} = require('./generateHtml'); //? imported incorrectly
+const { generateHTML } = require('./generateHtml'); //? imported incorrectly
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+let team = []; //holds all info for the team
 
 //Starts asking to create employees
 const startProgram = () => {
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'employee',
-      message: 'Which employee do you want to create?',
-      choices: ['Manager', 'Engineer', 'Intern', 'None, I am finished.']
-    }
-  ])
-  .then((choice) => {
-    console.log(choice)
-    //const htmlPageContent = generateHTML(answers);
-
-    // fs.writeFile('index.html', htmlPageContent, (err) =>
-    //   err ? console.log(err) : console.log('Successfully created index.html!')
-    // );
-  })
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'employee',
+        message: 'Which employee do you want to create?',
+        choices: ['Manager', 'Engineer', 'Intern', 'None, I am finished.']
+      }
+    ])
+    .then((choice) => {
+      console.log(choice)
+      if (choice.employee === 'Manager') {
+        createManager();
+      } else if (choice.employee === 'Intern'){
+        createIntern();
+      }
+    })
 }
+
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Manager name.'
+      }
+    ])
+    .then(function (input) {
+      console.log(input);
+      let newManager = new Manager(
+        //team.length,
+        input.name
+      )
+      team.push(newManager);
+      console.log(team);
+      startProgram()
+    })
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter intern name.'
+      }
+    ])
+    .then(function(input){
+      let newIntern = new Intern(
+        //team.length,
+        input.name
+      )
+      team.push(newIntern);
+      console.log(team)
+      startProgram();
+    })
+}
+
+
 startProgram();
 
-// If startProgram choice is Manager --> run new Manager ???
-// If choice is Intern --> run 
+
+//! put generate HTML function here
+
+//!
