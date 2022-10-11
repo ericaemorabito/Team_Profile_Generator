72375c75a3c8d60e97d21520dfbaa1956835857b
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-10-11 12:03:43 -0500

    CSS styles updated

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -12,7 +12,7 @@ const createEmployeeCard = (team) => {
       newManagerCard =
         `<div class="employee_card">
           <h2 class="name">${team[i].getName()}</h2>
-          <p class="job">${team[i].constructor.name}</p>
+          <p class="job">${team[i].constructor.name} <i class="fa-solid fa-briefcase"></i></p>
           <p class="id">ID: ${team[i].getId()}</p>
           <p class="email">Email: <a href="${team[i].getEmail()}" target="_blank">${team[i].getEmail()}</a></p>
           <p class="unique">Office: ${team[i].getOfficeNumber()}</p>
@@ -21,7 +21,7 @@ const createEmployeeCard = (team) => {
       newEngineerCard =
         `<div class="employee_card">
         <h2 class="name">${team[i].getName()}</h2>
-        <p class="job">${team[i].constructor.name}</p>
+        <p class="job">${team[i].constructor.name} <i class="fa-solid fa-computer"></i></p>
         <p class="id">ID: ${team[i].getId()}</p>
         <p class="email">Email: <a href="${team[i].getEmail()}" target="_blank">${team[i].getEmail()}</a></p>
         <p class="unique">Github: <a href="https://github.com/${team[i].getGithub()}" target="_blank">${team[i].getGithub()}</a></p>
@@ -30,9 +30,9 @@ const createEmployeeCard = (team) => {
       newInternCard = 
         `<div class="employee_card">
         <h2 class="name">${team[i].getName()}</h2>
-        <p class="job">${team[i].constructor.name}</p>
+        <p class="job">${team[i].constructor.name} <i class="fa-solid fa-user-graduate"></i></p>
         <p class="id">ID: ${team[i].getId()}</p>
-        <p class="email">Email: <a href="${team[i].getEmail()}" target="_blank>${team[i].getEmail()}</a></p>
+        <p class="email">Email: <a href="${team[i].getEmail()}" target="_blank">${team[i].getEmail()}</a></p>
         <p class="unique">School: ${team[i].getSchool()}</p>
         </div>`
     } else {
@@ -48,6 +48,10 @@ const createEmployeeCard = (team) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
+    <script src="https://kit.fontawesome.com/fa2acf3ead.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,500;1,400;1,500&family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
     <title>Meet the Team</title>
   </head>
   <body>
@@ -55,7 +59,8 @@ const createEmployeeCard = (team) => {
       <h1>Meet the Team</h1>
       <p id="header_info">We're happy to have a mix of creative and analytical thinkers. Meet our talented team members.</p>
     </header>
-  
+    <hr>
+
     <!-- Display employee cards area-->
     <section id="employee_area">
     ${newManagerCard}
